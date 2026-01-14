37773e6728d57b5bf2e2a5ac789305040595ace8
tthiagosilva <160064804+tthiagosilva@users.noreply.github.com>
2026-01-13 23:45:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -27,3 +27,20 @@ form.addEventListener("submit", async (e) => {
     btn.innerText = "Entrar na lista de espera";
   }
 });
+
+const telefoneInput = document.getElementById("telefone");
+
+telefoneInput.addEventListener("input", function (e) {
+  let valor = e.target.value.replace(/\D/g, "");
+
+  // limita a 11 dígitos (DDD + 9 números)
+  if (valor.length > 11) valor = valor.slice(0, 11);
+
+  if (valor.length > 6) {
+    e.target.value = `(${valor.slice(0, 2)})${valor.slice(2, 7)}-${valor.slice(7)}`;
+  } else if (valor.length > 2) {
+    e.target.value = `(${valor.slice(0, 2)})${valor.slice(2)}`;
+  } else if (valor.length > 0) {
+    e.target.value = `(${valor}`;
+  }
+});
